a6c79df513b63936738fb3b4071f1a9a9a5d6527
Manish Kumar <138682286+krmanish1@users.noreply.github.com>
2024-04-18 22:57:34 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 
 export const environment = {
     development: true,
-    BASE_URL: "http://localhost:3000/",
+    // BASE_URL: "http://localhost:3000/",
+    BASE_URL: "https://quick-notes-server-l7k46lj0t-krmanish1s-projects.vercel.app/",
+
 
 };
